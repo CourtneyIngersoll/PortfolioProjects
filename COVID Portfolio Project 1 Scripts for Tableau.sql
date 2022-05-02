d14f44c34cc79388c45ed00067875a1a8700184c
CourtneyIngersoll <104738826+CourtneyIngersoll@users.noreply.github.com>
2022-05-02 11:54:44 -0500

    Add files via upload
    SQL queries that were modified and selected for Tableau Dashboard (1): 
    
    https://public.tableau.com/app/profile/courtney.ingersoll/viz/COVIDPortfolioProject-InfectionsandDeaths/Dashboard1?publish=yes

---
+++ COVID Portfolio Project 1 Scripts for Tableau.sql
@@ -0,0 +1,37 @@
+/*Tableau Queries
+Simplified and Cleaned from COVID Portfolio for Vizulization Use*/
+
+-- 1.Glabal Numbers
+
+Select SUM(cast(new_cases as float)) as total_cases, SUM(cast(new_deaths as float)) as total_deaths, SUM(cast(new_deaths as float))/SUM(cast(New_Cases as float))*100 as DeathPercentage
+From PortfolioProject1..COVID_DEATHS
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+--2. Continents with the highest death per populaiton
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject1..COVID_DEATHS
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International', 'Upper middle income','High income', 'Lower middle income', 'Low income')
+Group by location
+order by TotalDeathCount desc
+
+--3.Contries with Highest Infection Rates Compared to Population 
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject1..COVID_DEATHS
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+--4.Contries with Highest Infection Rates Compared to Population by Date
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject1..COVID_DEATHS
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
